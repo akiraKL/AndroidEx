516fdb533441f72fa9a7420166ce65b0e17174e8
kim ohbeom <jazzkim12@naver.com>
2018-12-07 15:15:01 +0900

    App Init

---
+++ Chapter1App/Chapter1App/Chapter1App/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Chapter1App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Content = new Label
+            {
+                Text = "HI Xamarin.Forms!"
+            };
+
+            Padding = new Thickness(0, 40, 0, 0);
+        }
+    }
+}
